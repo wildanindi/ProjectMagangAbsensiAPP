5c9227805479e79d3bc4bb7bd7e536b25e8f8643
wildanindi2@gmail.com <wildanindi2@gmail.com>
2026-02-09 11:08:10 +0700

    menambah struktur data MVC

---
+++ backend/config/db.js
@@ -0,0 +1,22 @@
+const mysql = require('mysql2');
+require('dotenv').config();
+
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT || 3306,
+    
+});
+
+db.getConnection((err, connection) => {
+    if (err) {
+        console.error('Gagal terhubung ke database:', err);
+        return;
+    }
+    console.log('Berhasil terhubung ke database.');
+    connection.release();
+});
+
+module.exports = pool.promise();
